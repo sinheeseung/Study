66e67a8b2772baf3b39017ce8bde4d6faa94f27f
heeseung <tlsgmltmd726529@gmail.com>
2022-01-27 23:06:50 +0900

    2page Crawling

--- Crawling/해외 축구선수 몸값/TransferMarkt.py
+++ Crawling/해외 축구선수 몸값/TransferMarkt.py
@@ -4,15 +4,8 @@ import pandas as pd
 import time
 
 # requsts.get()으로 url정보 요청하기
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
-
-url = 'https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop'
-
-response = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-player_info = soup.findAll('tr', class_=['odd', 'even'])
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
 
 number = []
 name = []
@@ -22,15 +15,21 @@ nation = []
 team = []
 value = []
 
-for info in player_info:
-    player = info.find_all("td")
-    number.append(player[0].text)
-    name.append(player[3].text)
-    position.append(player[4].text)
-    age.append(player[5].text)
-    nation.append(player[6].img['alt'])  # img 태그의 alt 속성
-    team.append(player[7].img['alt'])
-    value.append(player[8].text.strip())
+for i in range(2):
+    url = f'https://www.transfermarkt.com/marktwertetop/wertvollstespieler?page={i}'
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    player_info = soup.findAll('tr', class_=['odd', 'even'])
+
+    for info in player_info:
+        player = info.find_all("td")
+        number.append(player[0].text)
+        name.append(player[3].text)
+        position.append(player[4].text)
+        age.append(player[5].text)
+        nation.append(player[6].img['alt'])  # img 태그의 alt 속성
+        team.append(player[7].img['alt'])
+        value.append(player[8].text.strip())
 
 df = pd.DataFrame(
     {"number": number,
@@ -44,4 +43,4 @@ df = pd.DataFrame(
 
 print(df)
 
-df.to_csv('TransferMarkt25.csv', index=False)
+df.to_csv('TransferMarkt50.csv', index=False)
